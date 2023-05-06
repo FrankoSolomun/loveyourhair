956fb00cfe28c69a0ac5e952dcea46d4f1208bec
FrankoSolomun <frankosolomun067@gmail.com>
2023-05-06 17:28:58 +0200

    aa

--- front/components/urls.js
+++ front/components/urls.js
@@ -1,4 +1,4 @@
-export const API_URL =     process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:1337'
+export const API_URL =     process.env.NEXT_PUBLIC_API_URL || 'https://api.loveyourhair.hr'
 
 
 /**
